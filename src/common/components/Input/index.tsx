cb617e2f93826b97a38533814089352774c6927c
Adnan Dervisevic <adnan.dervisevic@collectorbank.se>
2018-03-15 14:55:55 +0100

    Change the style of the input error to be more like the Collector Bank App & stylish

--- src/common/components/Input/index.tsx
+++ src/common/components/Input/index.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
+import Collapse from 'react-css-collapse';
 import glamorous, { CSSProperties, GlamorousComponent } from 'glamorous';
 import * as uniqid from 'uniqid';
 import { borderRadius, colors } from '../../../theme';
-import { Alert } from '../Alert';
 import { Label } from '../Label';
+import { css } from 'glamor';
 
 export const InputContainer: GlamorousComponent<React.HTMLProps<HTMLDivElement>, {}> = glamorous.div({
     maxWidth: 500,
@@ -16,7 +17,7 @@ const inputStyles: CSSProperties = {
     width: '100%',
     borderRadius: borderRadius.small,
     border: '1px solid',
-    padding: 11,
+    padding: 11, // To make it 12 pixels when accounting for the 1px border above
     boxSizing: 'border-box',
     appearance: 'none',
 
@@ -27,12 +28,36 @@ const inputStyles: CSSProperties = {
     },
 };
 
+const transition = css({
+    transition: 'height 150ms',
+});
+
+const inputErrorStyles: CSSProperties = {
+    background: colors.red,
+    fontWeight: 600,
+    borderBottomLeftRadius: borderRadius.small,
+    borderBottomRightRadius: borderRadius.small,
+    color: colors.white,
+    padding: 8,
+    paddingLeft: 12,
+    maxHeight: 0,
+    transition: 'max-height 150ms',
+}
+
 export const InputField = glamorous.input<{ hasError?: boolean }>(
     inputStyles,
     ({ hasError }) => ({
-        borderColor: hasError ? colors.red : colors.mediumGray,
-        backgroundColor: hasError ? '#FFFCFC' : colors.white,
-    })
+        borderBottomLeftRadius: hasError ? 0 : borderRadius.small,
+        borderBottomRightRadius: hasError ? 0 : borderRadius.small,
+        borderBottomColor: hasError ? colors.red : colors.black
+    }),
+);
+
+export const InputError = glamorous.div<{ hasError?: boolean }>(
+    inputErrorStyles,
+    ({ hasError }) => ({
+        maxHeight: '100%',
+    }),
 );
 
 const Textarea = glamorous.textarea<{ hasError?: boolean }>(
@@ -96,9 +121,10 @@ export class Input extends React.Component<InputProps, InputState> {
                     aria-invalid={showError}
                     {...rest}
                 />
-                {showError && typeof error === 'string' &&
-                    <Alert type="error" message={error} alertSize="small" />
-                }
+
+                <Collapse isOpen={showError && typeof error === 'string'} className={`${transition}`}>
+                    <InputError hasError={Boolean(error)}> {error} </InputError>
+                </Collapse>
             </InputContainer>
         );
     }

a309bacf009bc7a6ab102981e7f7857fa29c3d43
Adnan Dervisevic <adnan.dervisevic@collectorbank.se>
2018-12-03 09:29:32 +0100

    Remove border and margin in the top of the sub body of the card container
    Doesn't match our new design specifications, makes it easier to layout and actually use the sub body

--- src/common/components/Card/index.tsx
+++ src/common/components/Card/index.tsx
@@ -4,50 +4,36 @@ import { Theme } from '../../../themes';
 
 const cross = `'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="14" height="14"%3E%3Cdefs%3E%3Cpath id="a" d="M13.4142136 12l5.2928932 5.2928932c.3905243.3905243.3905243 1.0236893 0 1.4142136-.3905243.3905243-1.0236893.3905243-1.4142136 0L12 13.4142136l-5.29289322 5.2928932c-.39052429.3905243-1.02368927.3905243-1.41421356 0-.39052429-.3905243-.39052429-1.0236893 0-1.4142136L10.5857864 12 5.29289322 6.70710678c-.39052429-.39052429-.39052429-1.02368927 0-1.41421356.39052429-.39052429 1.02368927-.39052429 1.41421356 0L12 10.5857864l5.2928932-5.29289318c.3905243-.39052429 1.0236893-.39052429 1.4142136 0 .3905243.39052429.3905243 1.02368927 0 1.41421356L13.4142136 12z"/%3E%3C/defs%3E%3Cg fill="none" fill-rule="evenodd" transform="translate(-5 -5)"%3E%3Cmask id="b" fill="%23fff"%3E%3Cuse xlink:href="%23a"/%3E%3C/mask%3E%3Cuse fill="%23000" fill-rule="nonzero" xlink:href="%23a"/%3E%3Cg fill="%23000" mask="url(%23b)"%3E%3Cpath d="M0 0h24v24H0z"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E'`;
 
-const CardContainer = glamorous.div<{ theme: Theme }>(
-    {
-        display: 'block',
-        boxShadow: '0 2px 7px 0 rgba(0, 0, 0, 0.2)',
-        boxSizing: 'border-box',
-        width: 280,
-        padding: 20,
-        marginBottom: 20,
-        color: 'inherit',
-        textDecoration: 'none',
-        minHeight: 200,
-    },
-    ({ theme }) => ({
-        borderRadius: theme.borderRadius.large,
-        background: theme.colors.white,
-    })
-);
+const CardContainer = glamorous.div<{ theme: Theme }>(({ theme }) => ({
+    display: 'block',
+    boxShadow: '0 2px 7px 0 rgba(0, 0, 0, 0.2)',
+    boxSizing: 'border-box',
+    width: 280,
+    padding: 20,
+    marginBottom: 20,
+    color: 'inherit',
+    textDecoration: 'none',
+    minHeight: 200,
+    borderRadius: theme.borderRadius.large,
+    background: theme.colors.white,
+}));
 
-const Heading = glamorous.div<{ color: CardColors; theme: Theme }>(
-    {
-        display: 'inline-block',
-        fontWeight: 600,
-        marginBottom: 16,
-        borderBottom: '3px solid',
-    },
-    ({ color, theme }) => ({
-        borderColor: color ? theme.colors[color] : 'transparent',
-    })
-);
+const Heading = glamorous.div<{ color: CardColors; theme: Theme }>(({ color, theme }) => ({
+    display: 'inline-block',
+    fontWeight: 600,
+    marginBottom: 16,
+    borderBottom: '3px solid',
+    borderColor: color ? theme.colors[color] : 'transparent',
+}));
 
 const Body = glamorous.div({
     overflowWrap: 'break-word',
 });
 
-const SubBody = glamorous.div<{ theme: Theme }>(
-    {
-        borderTop: '1px solid',
-        marginTop: 16,
-        paddingTop: 16,
-    },
-    ({ theme }) => ({
-        borderColor: theme.colors.lightGray,
-    })
-);
+const SubBody = glamorous.div<{ theme: Theme }>(({ theme }) => ({
+    paddingTop: 16,
+    borderColor: theme.colors.lightGray,
+}));
 
 const Dismissable = glamorous.div({
     width: 14,

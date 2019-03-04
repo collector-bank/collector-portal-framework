2ff115d3bfb12f14332f59bb8e2b8004f5669f24
Jonathan Persson <jonathan.persson@collectorbank.se>
2019-03-04 11:37:12 +0100

    Adjust the size of Button component

--- src/common/components/Button/index.tsx
+++ src/common/components/Button/index.tsx
@@ -7,10 +7,7 @@ import styled from '../../../';
 /**
  * The SVG was made with http://loading.io
  */
-const spinner = (color: string) =>
-    `'data:image/svg+xml,%3Csvg width=%2280%22 height=%2280%22 xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22 preserveAspectRatio=%22xMidYMid%22 class=%22uil-ring-alt%22%3E%3Cpath class=%22bk%22 fill=%22none%22 d=%22M0 0h100v100h-100z%22/%3E%3Ccircle cx=%2250%22 cy=%2250%22 r=%2240%22 fill=%22none%22/%3E%3Ccircle cx=%2250%22 cy=%2250%22 r=%2240%22 stroke=%22${encodeURIComponent(
-        color
-    )}%22 stroke-width=%226%22 stroke-linecap=%22round%22 fill=%22none%22%3E%3Canimate attributeName=%22stroke-dashoffset%22 dur=%222s%22 repeatCount=%22indefinite%22 from=%220%22 to=%22502%22/%3E%3Canimate attributeName=%22stroke-dasharray%22 dur=%222s%22 repeatCount=%22indefinite%22 values=%22150.6 100.4;1 250;150.6 100.4%22/%3E%3C/circle%3E%3C/svg%3E'`;
+const spinner = (color: string) => `'data:image/svg+xml,%3Csvg width=%2280%22 height=%2280%22 xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22 preserveAspectRatio=%22xMidYMid%22 class=%22uil-ring-alt%22%3E%3Cpath class=%22bk%22 fill=%22none%22 d=%22M0 0h100v100h-100z%22/%3E%3Ccircle cx=%2250%22 cy=%2250%22 r=%2240%22 fill=%22none%22/%3E%3Ccircle cx=%2250%22 cy=%2250%22 r=%2240%22 stroke=%22${encodeURIComponent(color)}%22 stroke-width=%226%22 stroke-linecap=%22round%22 fill=%22none%22%3E%3Canimate attributeName=%22stroke-dashoffset%22 dur=%222s%22 repeatCount=%22indefinite%22 from=%220%22 to=%22502%22/%3E%3Canimate attributeName=%22stroke-dasharray%22 dur=%222s%22 repeatCount=%22indefinite%22 values=%22150.6 100.4;1 250;150.6 100.4%22/%3E%3C/circle%3E%3C/svg%3E'`;
 
 export type ButtonType = 'primary' | 'secondary' | 'secondaryNegative' | 'success' | 'warn' | 'text';
 export type ButtonSize = 'small' | 'medium' | 'large';
@@ -136,6 +133,18 @@ const getTypeStyles = (theme: Theme, type?: ButtonType) => {
 };
 
 const getSizeStyles = (theme: Theme, size?: ButtonSize): CSSObject => {
+    const small: CSSObject = {
+        fontSize: 16,
+        lineHeight: '24px',
+        fontWeight: 500,
+        minWidth: 80,
+        paddingTop: 6,
+        paddingBottom: 6,
+        paddingLeft: 16,
+        paddingRight: 16,
+        backgroundSize: 22, // the loading indicator
+    };
+
     const medium: CSSObject = {
         fontSize: 18,
         lineHeight: '24px',
@@ -145,36 +154,35 @@ const getSizeStyles = (theme: Theme, size?: ButtonSize): CSSObject => {
         paddingBottom: 12,
         paddingLeft: 24,
         paddingRight: 24,
+        backgroundSize: 36, // the loading indicator
+    };
+
+    const large: CSSObject = {
+        fontSize: 21,
+        lineHeight: '32px',
+        fontWeight: 500,
+        minWidth: 200,
+        paddingTop: 12,
+        paddingBottom: 12,
+        paddingLeft: 32,
+        paddingRight: 32,
+        backgroundSize: 46, // the loading indicator
     };
 
     switch (size) {
         case 'small':
-            return {
-                fontSize: 16,
-                lineHeight: '24px',
-                fontWeight: 500,
-                minWidth: 80,
-                paddingTop: 4,
-                paddingBottom: 4,
-                paddingLeft: 12,
-                paddingRight: 12,
-            };
+            return small;
         case 'large':
             return {
-                fontSize: 21,
-                lineHeight: '32px',
-                fontWeight: 500,
-                minWidth: 200,
-                paddingTop: 12,
-                paddingBottom: 12,
-                paddingLeft: 32,
-                paddingRight: 32,
-
+                ...large,
                 [theme.breakpoints.mobileAndLower]: medium,
             };
         case 'medium':
         default:
-            return medium;
+            return {
+                ...medium,
+                [theme.breakpoints.mobileAndLower]: small,
+            };
     }
 };
 
@@ -207,19 +215,6 @@ const getLoadingStyles = (theme: Theme, loading?: boolean, type?: ButtonType, si
             case 'primary':
             default:
                 styles.backgroundImage = `url(${spinner(theme.colors.white)})`;
-                break;
-        }
-
-        switch (size) {
-            case 'small':
-                styles.backgroundSize = 22;
-                break;
-            case 'large':
-                styles.backgroundSize = 46;
-                break;
-            case 'medium':
-            default:
-                styles.backgroundSize = 36;
                 break;
         }
     }

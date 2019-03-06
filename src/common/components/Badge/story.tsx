9a8c1a0d6fdb11e00f5125b2c3d48e6e59d5fb8e
Adnan Dervisevic <adnan.dervisevic@collectorbank.se>
2019-03-06 09:47:44 +0100

    Upgrade to storybook 5 but wait for npm install to work properly

--- src/common/components/Badge/story.tsx
+++ src/common/components/Badge/story.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text, select } from '@storybook/addon-knobs';
-import { withReadme } from 'storybook-readme';
 import { Badge, BadgeColor } from './';
-import readme from './README.md';
+import notes from './README.md';
 
 const components = storiesOf('Components', module);
 
 components.addDecorator(withKnobs);
 
-components.add('Badge', withReadme(readme, () => {
-    const colors: BadgeColor[] = ['primary', 'yellow', 'green', 'red', 'blue', 'lightGray'];
+components.add(
+    'Badge',
+    () => {
+        const colors: BadgeColor[] = ['primary', 'yellow', 'green', 'red', 'blue', 'lightGray'];
 
-    return (
-        <Badge
-            label={text('Label', 'En badge')}
-            color={select('Color', colors, 'primary')}
-            tooltip={text('Tooltip', 'En tooltip')}
-        />
-    );
-}));
+        return (
+            <Badge label={text('Label', 'En badge')} color={select('Color', colors, 'primary')} tooltip={text('Tooltip', 'En tooltip')} />
+        );
+    },
+    { notes }
+);

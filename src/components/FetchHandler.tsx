23b6851001ec3655a1b46aaec2a2bdb1de1dd43e
Adnan Dervisevic <adnan.dervisevic@collectorbank.se>
2019-03-20 18:47:46 +0100

    Proposal for improving usability of fetch handler

--- src/components/FetchHandler.tsx
+++ src/components/FetchHandler.tsx
@@ -1,28 +1,33 @@
 import * as React from 'react';
-import { usePromise } from '../hooks/use-promise';
-import { Spinner, Alert } from '../common/components';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { usePromise } from '../hooks';
+import { Spinner, Alert } from '.';
 
 interface Props<TResult> {
     apiMethod: () => Promise<TResult>;
-    children: (data: TResult) => React.ReactNode;
+    children: (data: TResult, updateDependencies?: (dependencies: any) => void) => React.ReactNode;
     errorText: string;
     errorIndicator?: React.ReactNode;
     loadingIndicator?: React.ReactNode;
 }
 
-export const FetchHandler = <TResult extends {}>({ apiMethod, children, loadingIndicator, errorIndicator, errorText }: Props<TResult>) => {
+export const FetchHandler = <TResult extends {}>({ apiMethod, children, errorText, errorIndicator, loadingIndicator }: Props<TResult>) => {
     const { loading, data, error, trigger } = usePromise(apiMethod);
+    const [dependencyList, setDependencyList] = useState(undefined);
 
     useEffect(() => {
         trigger();
-    }, []);
+
+        if (dependencyList) {
+            setDependencyList(undefined);
+        }
+    }, [dependencyList, trigger]);
 
     return (
         <>
             {loading && (loadingIndicator ? loadingIndicator : <Spinner centered />)}
             {error && (errorIndicator ? errorIndicator : <Alert type="error" message={errorText} />)}
-            {data && children(data)}
+            {data && children(data, (dependencies: any) => setDependencyList(dependencies))}
         </>
     );
 };

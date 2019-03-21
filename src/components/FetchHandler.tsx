008081c3b160e92aca5a402c9575b3d3408a5c05
Adnan Dervisevic <adnan.dervisevic@collectorbank.se>
2019-03-21 15:17:21 +0100

    Fix bug with dependency list in FetchHandler

--- src/components/FetchHandler.tsx
+++ src/components/FetchHandler.tsx
@@ -8,7 +8,7 @@ import { Spinner, Alert } from '.';
  * @param setDependencyList This is a callback which you use to set the list of dependencies for the useEffect hook.
  * Using this will allow you to re-fetch the data if you have a dependency on a submit for example.
  */
-type Child<TResult> = (data: TResult, setDependencyList: (dependencies: any[]) => void) => React.ReactNode;
+type Child<TResult> = (data: TResult, setDependencyList: (dependencies: any) => void) => React.ReactNode;
 
 interface Props<TResult> {
     apiMethod: () => Promise<TResult>;
@@ -20,11 +20,11 @@ interface Props<TResult> {
 
 export const FetchHandler = <TResult extends {}>({ apiMethod, children, errorText, errorIndicator, loadingIndicator }: Props<TResult>) => {
     const { loading, data, error, trigger } = usePromise(apiMethod);
-    const [dependencyList, setDependencyList] = useState<any[]>([]);
+    const [dependencyList, setDependencyList] = useState(undefined);
 
     useEffect(() => {
         trigger();
-    }, [...dependencyList]);
+    }, [dependencyList, trigger]);
 
     return (
         <>

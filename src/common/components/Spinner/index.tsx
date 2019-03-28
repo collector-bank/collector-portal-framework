c5f1824fccb8c60d78e219df36d4924bc7f97717
Jonathan Persson <jonathan.persson@collectorbank.se>
2019-03-28 17:02:50 +0100

    Use SVG animations for animating the loading spinner instead of SMIL

--- src/common/components/Spinner/index.tsx
+++ src/common/components/Spinner/index.tsx
@@ -1,42 +1,70 @@
+import React from 'react';
 import { keyframes, CSSObject } from '@emotion/core';
+import { withTheme } from 'emotion-theming';
 import styled from '../../../';
+import { Theme } from '../../../themes';
 
-/**
- * The SVG was made with http://loading.io
- */
+const animation = keyframes({
+    '0%': {
+        strokeDashoffset: '0',
+        strokeDasharray: '150 100',
+    },
+    
+    '50%': {
+        strokeDasharray: '1 250'
+    },
+    
+    '100%': {
+        strokeDashoffset: '502',
+        strokeDasharray: '150 100'
+    }
+});
 
-const spinner = (color: string) =>
-    `'data:image/svg+xml, %3Csvg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22 preserveAspectRatio=%22xMidYMid%22%3E%3Cpath fill=%22none%22 d=%22M0 0h100v100h-100z%22 /%3E%3Ccircle cx=%2250%22 cy=%2250%22 r=%2240%22 fill=%22none%22 /%3E%3Ccircle cx=%2250%22 cy=%2250%22 r=%2240%22 stroke=%22${encodeURIComponent(
-        color
-    )}%22 stroke-width=%226%22 stroke-linecap=%22round%22 fill=%22none%22%3E%3Canimate attributeName=%22stroke-dashoffset%22 dur=%222s%22 repeatCount=%22indefinite%22 from=%220%22 to=%22502%22 /%3E%3Canimate attributeName=%22stroke-dasharray%22 dur=%222s%22 repeatCount=%22indefinite%22 values=%22150.6 100.4;1 250;150.6 100.4%22 /%3E%3C/circle%3E%3C/svg%3E'`;
+const AnimatedCircle = styled.circle({
+    animation: `${animation} 2s linear infinite`,
+    fill: 'none',
+});
+
+export const SpinnerSvg: React.StatelessComponent<{ color: string; size: string | number }> = ({ color, size }) => (
+    <svg width={size} height={size} viewBox="0 0 100 100">
+        <AnimatedCircle cx={50} cy={50} r={40} />
+        <AnimatedCircle cx={50} cy={50} r={40} stroke={color} strokeWidth={6} strokeLinecap="round" />
+    </svg>
+);
 
 const fadeIn = keyframes({
     '0%': { opacity: 0 },
     '100%': { opacity: 1 },
 });
 
-export interface SpinnerProps {
-    size?: number;
-    centered?: boolean;
-    color?: string;
-}
-
-export const Spinner = styled.div<SpinnerProps>(({ theme, size = 80, centered = false }) => {
+const Container = styled.div<SpinnerProps>(({ size, centered }) => {
     const styles: CSSObject = {
         display: 'inline-block',
         animation: `${fadeIn} 250ms ease-in-out`,
         width: size,
         height: size,
-        background: `url(${spinner(theme.colors.primary)}) no-repeat`,
     };
 
     if (centered) {
         styles.position = 'absolute';
-        styles.top = `calc(50% - ${size / 2}px)`;
-        styles.left = `calc(50% - ${size / 2}px)`;
+        styles.top = `calc(50% - ${size! / 2}px)`;
+        styles.left = `calc(50% - ${size! / 2}px)`;
     }
 
     return styles;
 });
+
+export interface SpinnerProps {
+    size?: number;
+    centered?: boolean;
+}
+
+const Spinner_: React.StatelessComponent<SpinnerProps & { theme: Theme }> = ({ theme, size = 80, centered = false }) => (
+    <Container size={size} centered={centered}>
+        <SpinnerSvg size={size} color={theme.colors.primary} />
+    </Container>
+);
+
+export const Spinner = withTheme(Spinner_);
 
 Spinner.displayName = 'Collector.Spinner';

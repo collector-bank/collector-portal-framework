1d8d0b044552e51b6e9991b479eb772a7194b140
Adnan Dervisevic <adnan.dervisevic@collectorbank.se>
2019-05-21 11:08:54 +0200

    Revert changes from experimental branch

--- src/common/typography/Subtitle.tsx
+++
@@ -1,3 +0,0 @@
-import styled from '../../';
-
-export const Subtitle = styled.span<{}>(({ theme }) => ({ color: '#707070', fontSize: theme.fonts.desktop.small }));
--- src/common/typography/index.ts
+++ src/common/typography/index.ts
@@ -3,4 +3,3 @@ export * from './H2';
 export * from './H3';
 export * from './Text';
 export * from './TextLink';
-export * from './Subtitle';

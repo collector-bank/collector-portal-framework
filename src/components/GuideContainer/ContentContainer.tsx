cb465bf252cde4714498888bca2b27adc32ce206
Niklas Westman <niklaswestman@Niklass-MacBook-Pro.local>
2021-03-18 10:56:51 +0100

    update guidecontainer

--- src/components/GuideContainer/ContentContainer.tsx
+++ src/components/GuideContainer/ContentContainer.tsx
@@ -1,18 +1,9 @@
 import styled from '../..';
 
 export const GuideContentContainer = styled.div<{ maxWidth?: number }>(({ theme, maxWidth }) => ({
-    padding: 40,
     paddingBottom: 'calc(40px - 1.25em)',
     borderRadius: theme.borderRadius.small,
     background: theme.colors.white,
     maxWidth: maxWidth ? maxWidth : undefined,
     margin: maxWidth ? '0 auto 24px' : '0 0 24px',
-
-    [theme.breakpoints.tabletAndLower]: {
-        padding: 20,
-    },
-
-    [theme.breakpoints.mobileAndLower]: {
-        padding: 16,
-    },
 }));
